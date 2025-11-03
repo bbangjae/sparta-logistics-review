64487f0f4db93fdfdacd576e988a3b3180328025
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-11-03 15:35:35 +0900

    환경설정

---
+++ services/delivery-server/src/test/java/com/sparta/deliveryserver/DeliveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.deliveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
