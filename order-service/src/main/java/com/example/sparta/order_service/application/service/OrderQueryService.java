461b9409c353b37df1de9a44ac762ae10e773ea7
rudcjs <rudcjs335@gmail.com>
2025-11-07 07:14:27 +0900

    Fix: 페이징 관련 예외 처리

--- order-service/src/main/java/com/example/sparta/order_service/application/service/OrderQueryService.java
+++ order-service/src/main/java/com/example/sparta/order_service/application/service/OrderQueryService.java
@@ -1,5 +1,7 @@
 package com.example.sparta.order_service.application.service;
 
+import com.example.sparta.common.exception.BusinessException;
+import com.example.sparta.common.exception.ErrorCode;
 import com.example.sparta.order_service.domain.repository.OrderQueryRepository;
 import com.example.sparta.order_service.presentation.dto.request.SearchCondition;
 import com.example.sparta.order_service.presentation.dto.response.OrderResponse;
@@ -17,6 +19,8 @@ public class OrderQueryService {
 
     // TODO 사용자 권한에 따른 verify 절차 필요
     public Page<OrderResponse> search(SearchCondition condition, String userEmail, Pageable pageable) {
+        if(pageable.getPageSize() < 0 || pageable.getPageNumber() < 0)
+            throw new BusinessException(ErrorCode.INVALID_INPUT_VALUE, "페이지 사이즈와 번호는 0 이상이어야합니다.");
         return queryRepository.search(condition, userEmail, pageable);
     }
 }
