8f1b40d0406b8e5f519268786b183b07308fc8b2
bbangjae <120161896+bbangjae@users.noreply.github.com>
2025-11-06 10:24:12 +0900

    @Component 주석 제거
    @Component 주석 제거

--- common/src/main/java/com/example/sparta/common/config/AuditorAwareImpl.java
+++ common/src/main/java/com/example/sparta/common/config/AuditorAwareImpl.java
@@ -4,9 +4,9 @@ import java.util.Optional;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class AuditorAwareImpl implements AuditorAware<Long> {
-    @Override
+    
     public Optional<Long> getCurrentAuditor() {
         // TODO: Spring Security 인증 구현 후 주석 해제
         /*
