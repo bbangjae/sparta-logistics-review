da37756f0bce5567a149e8ff3f8f07cac2b0b7ce
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2025-11-12 22:13:53 +0900

    refactor: header에 company_id, hub_id, delivery_id 추가

--- user-service/src/main/java/com/sparta/user_service/infrastructure/config/SecurityConfig.java
+++ user-service/src/main/java/com/sparta/user_service/infrastructure/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.sparta.user_service.infrastructure.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // CSRF 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // 모든 요청 허용
         return http.build();
     }
